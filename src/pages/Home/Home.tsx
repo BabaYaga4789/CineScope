bbe9c4872e7139eca760fbfbbb868eef1fa9cd53
Hrishi <hr581440@dal.ca>
2023-02-27 20:20:16 -0400

    fix layout, add labels to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import FilterDropdown from "@/components/FilterDropdown";
 import MovieGridItem from "@/components/MovieGridItem";
 import SearchBar from "@/components/SearchBar";
-import { SimpleGrid, VStack } from "@chakra-ui/react";
+import { SimpleGrid, VStack, Text } from "@chakra-ui/react";
 import { MovieDetails } from "../MovieData";
 
 export default function Home() {
@@ -14,6 +14,15 @@ export default function Home() {
       <SearchBar />
       <FilterDropdown />
       {/* Reference: https://chakra-ui.com/docs/components/simple-grid */}
+      <Text fontSize="2xl" fontWeight="bold" px={8} alignSelf="flex-start">
+        Top Movies
+      </Text>
+      <SimpleGrid p={4} w="100%" columns={{ base: 1, md: 3, lg: 7 }} gap={6}>
+        {movies}
+      </SimpleGrid>
+      <Text fontSize="2xl" fontWeight="bold" px={8} alignSelf="flex-start">
+        New Movies
+      </Text>
       <SimpleGrid p={4} w="100%" columns={{ base: 1, md: 3, lg: 7 }} gap={6}>
         {movies}
       </SimpleGrid>
