a0f55e19ca80a65944351c109dd36bd7b46c71e4
Hrishi <hr581440@dal.ca>
2023-02-28 13:06:44 -0400

    fix News

--- src/pages/News/News.tsx
+++ src/pages/News/News.tsx
@@ -7,150 +7,153 @@ import {
   CardHeader,
   Flex,
   Heading,
-  Image, LinkOverlay,
+  Image,
   Spacer,
   Stack,
   Text,
-  useToast
+  useToast,
 } from "@chakra-ui/react";
+import { Link, useNavigate } from "react-router-dom";
 
 const today = new Date();
 
 export default function News() {
-    const toast = useToast();
+  const toast = useToast();
+  const navigate = useNavigate();
 
-    const onClick = (event: any) => {
-        event.preventDefault();
-        toast({
-            position: "bottom",
-            render: () => (
-                <Box color="white" p={3} bg="teal.500">
-                    Subscribed successfully!
-                </Box>
-            ),
-        });
-    };
-    return (
-        <><Flex as="nav" justify="space-between" paddingBottom={"4"}>
-            <Box>
-                <Heading
-                    p={"10px"}
-                    fontFamily={"heading"}
-                    color="Silver"
-                    fontSize="5xl"
-                >
-                    Top news
-                </Heading>
-            </Box>
-            <Box p={"30px"} display="flex" flexDirection={"row"}>
-                <Button
-                    colorScheme={"blue"}
-                    size="lg"
-                    variant="ghost"
-                    onClick={onClick}
-                >
-                    Subscribe
-                </Button>
-            </Box>
-        </Flex><Spacer /><Flex
-            pl="20px"
-            pr={"20px"}
-            display="flex"
-            flexDirection={"column"}
+  const onClick = (event: any) => {
+    event.preventDefault();
+    toast({
+      title: "Subscribed",
+      description: "You have subscribed to our newsletter successfully",
+      status: "success",
+      duration: 4000,
+      isClosable: true,
+    });
+  };
+  return (
+    <>
+      <Flex as="nav" justify="space-between" paddingBottom={"4"}>
+        <Box>
+          <Heading
+            p={"10px"}
+            fontFamily={"heading"}
+            color="Silver"
+            fontSize="5xl"
+          >
+            Top news
+          </Heading>
+        </Box>
+        <Box p={"30px"} display="flex" flexDirection={"row"}>
+          <Button
+            colorScheme={"yellow"}
+            size="lg"
+            variant="ghost"
+            onClick={onClick}
+          >
+            Subscribe
+          </Button>
+        </Box>
+      </Flex>
+      <Spacer />
+      <Flex pl="20px" pr={"20px"} display="flex" flexDirection={"column"}>
+        <Box
+          py={{ base: "0", sm: "8" }}
+          px={{ base: "4", sm: "10" }}
+          bg={{ base: "transparent", sm: "bg-surface" }}
+          boxShadow={{ base: "none", sm: "md" }}
+          borderRadius={{ base: "none", sm: "xl" }}
+          {...{ mt: "10px" }}
         >
+          <Card onClick={() => navigate("/news-details")} cursor="pointer">
+            <Heading
+              fontSize={"3xl"}
+              fontStyle="bold"
+              textAlign="left"
+              paddingBottom={"2"}
+            >
+              <Link to="/news-details">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              </Link>
+            </Heading>
+            <Stack>
+              <Image
+                objectFit={"cover"}
+                maxW={{ base: "100%", sm: "300px" }}
+                src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+                alt="image"
+              />
+            </Stack>
+            <Stack>
+              <CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                  fuga, non dolorem commodi eum sapiente accusantium dolorum
+                  error nulla excepturi. Maxime corrupti et quo, aspernatur
+                  fugiat quisquam nostrum placeat nobis! Lorem ipsum dolor sit
+                  amet consectetur adipisicing elit. Vero fuga, non dolorem
+                  commodi eum sapiente accusantium dolorum error nulla
+                  excepturi. Maxime corrupti et quo, aspernatur fugiat quisquam
+                  nostrum placeat nobis! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Vero fuga, non dolorem commodi eum sapiente
+                  accusantium dolorum error nulla excepturi. Maxime corrupti et
+                  quo, aspernatur fugiat quisquam nostrum placeat nobis!
+                </Text>
+              </CardBody>
 
-                <Box
-                    py={{ base: '0', sm: '8' }}
-                    px={{ base: '4', sm: '10' }}
-                    bg={{ base: 'transparent', sm: 'bg-surface' }}
-                    boxShadow={{ base: 'none', sm: 'md' }}
-                    borderRadius={{ base: 'none', sm: 'xl' }}
-                    {...{ mt: "10px" }}
-                >
-                    <Card>
-                        <Heading fontSize={"3xl"} fontStyle="bold" textAlign="left" paddingBottom={"2"}>
-                            <LinkOverlay href="/news-details">
-                                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                            </LinkOverlay>
-                        </Heading>
-                        <Stack>
+              <CardFooter>
+                <Text>Today is {today.toLocaleDateString()}</Text>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </Box>
 
-                            <Image
-                                objectFit={"cover"}
-                                maxW={{ base: "100%", sm: "300px" }}
-                                src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-                                alt="image" />
-                        </Stack>
-                        <Stack>
-                            <CardBody>
-                                <Text py="2">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                                    fuga, non dolorem commodi eum sapiente accusantium dolorum
-                                    error nulla excepturi. Maxime corrupti et quo, aspernatur
-                                    fugiat quisquam nostrum placeat nobis! Lorem ipsum dolor sit
-                                    amet consectetur adipisicing elit. Vero fuga, non dolorem
-                                    commodi eum sapiente accusantium dolorum error nulla
-                                    excepturi. Maxime corrupti et quo, aspernatur fugiat quisquam
-                                    nostrum placeat nobis! Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Vero fuga, non dolorem commodi eum sapiente
-                                    accusantium dolorum error nulla excepturi. Maxime corrupti et
-                                    quo, aspernatur fugiat quisquam nostrum placeat nobis!
-                                </Text>
-                            </CardBody>
+        <Box
+          py={{ base: "0", sm: "8" }}
+          px={{ base: "4", sm: "10" }}
+          bg={{ base: "transparent", sm: "bg-surface" }}
+          boxShadow={{ base: "none", sm: "md" }}
+          borderRadius={{ base: "none", sm: "xl" }}
+          {...{ mt: "10px" }}
+        >
+          <Card onClick={() => navigate("/news-details")} cursor="pointer">
+            <CardHeader>
+              <Heading fontSize={"3xl"} fontStyle="bold" textAlign="left">
+                <Link to="/news-details">
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                </Link>
+              </Heading>
+            </CardHeader>
+            <Image
+              objectFit={"cover"}
+              maxW={{ base: "100%", sm: "300px" }}
+              src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
+              alt="image"
+            />
+            <Stack>
+              <CardBody>
+                <Text py="2">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+                  fuga, non dolorem commodi eum sapiente accusantium dolorum
+                  error nulla excepturi. Maxime corrupti et quo, aspernatur
+                  fugiat quisquam nostrum placeat nobis! Lorem ipsum dolor sit
+                  amet consectetur adipisicing elit. Vero fuga, non dolorem
+                  commodi eum sapiente accusantium dolorum error nulla
+                  excepturi. Maxime corrupti et quo, aspernatur fugiat quisquam
+                  nostrum placeat nobis! Lorem ipsum dolor sit amet consectetur
+                  adipisicing elit. Vero fuga, non dolorem commodi eum sapiente
+                  accusantium dolorum error nulla excepturi. Maxime corrupti et
+                  quo, aspernatur fugiat quisquam nostrum placeat nobis!
+                </Text>
+              </CardBody>
 
-                            <CardFooter>
-                                <Text>Today is {today.toLocaleDateString()}</Text>
-                            </CardFooter>
-                        </Stack>
-                    </Card>
-                </Box>
-
-                <Box
-                    py={{ base: '0', sm: '8' }}
-                    px={{ base: '4', sm: '10' }}
-                    bg={{ base: 'transparent', sm: 'bg-surface' }}
-                    boxShadow={{ base: 'none', sm: 'md' }}
-                    borderRadius={{ base: 'none', sm: 'xl' }}
-                    {...{ mt: "10px" }}>
-                    <Card>
-                        <CardHeader>
-                            <Heading fontSize={"3xl"} fontStyle="bold" textAlign="left">
-                                <LinkOverlay href="/newsdetails">
-                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                                </LinkOverlay>
-                            </Heading>
-                        </CardHeader>
-                        <Image
-                            objectFit={"cover"}
-                            maxW={{ base: "100%", sm: "300px" }}
-                            src="https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60"
-                            alt="image" />
-                        <Stack>
-                            <CardBody>
-                                <Text py="2">
-                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
-                                    fuga, non dolorem commodi eum sapiente accusantium dolorum
-                                    error nulla excepturi. Maxime corrupti et quo, aspernatur
-                                    fugiat quisquam nostrum placeat nobis! Lorem ipsum dolor sit
-                                    amet consectetur adipisicing elit. Vero fuga, non dolorem
-                                    commodi eum sapiente accusantium dolorum error nulla
-                                    excepturi. Maxime corrupti et quo, aspernatur fugiat quisquam
-                                    nostrum placeat nobis! Lorem ipsum dolor sit amet consectetur
-                                    adipisicing elit. Vero fuga, non dolorem commodi eum sapiente
-                                    accusantium dolorum error nulla excepturi. Maxime corrupti et
-                                    quo, aspernatur fugiat quisquam nostrum placeat nobis!
-                                </Text>
-                            </CardBody>
-
-                            <CardFooter>
-                                <Text>Today is {today.toLocaleDateString()}</Text>
-                            </CardFooter>
-                        </Stack>
-                    </Card>
-                </Box>
-
-            </Flex></>
-
-
-    );
-}  
+              <CardFooter>
+                <Text>Today is {today.toLocaleDateString()}</Text>
+              </CardFooter>
+            </Stack>
+          </Card>
+        </Box>
+      </Flex>
+    </>
+  );
+}
