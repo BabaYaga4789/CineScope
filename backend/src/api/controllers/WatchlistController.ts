e8b79572f9e468fbd815b5cc677eff3c1bc2bd9e
Harsh Shah <hr838264@dal.ca>
2023-03-27 21:48:52 -0300

    Defined all the necessary funtions in Watchlist Controller

--- backend/src/api/controllers/WatchlistController.ts
+++ backend/src/api/controllers/WatchlistController.ts
@@ -3,13 +3,27 @@ import mongoose from "mongoose";
 import User from "../models/User";
 import Watchlist from "../models/Watchlist";
 
+// In a controller for maintaining a Watchlist for a user in Node,  you would typically need the following functions:
 
-const readWatchlist = (req: Request, res: Response, next: NextFunction) => {
-    
-};
+// getWatchlist: This function would retrieve the current watchlist for a given user.
+const getWatchlist = (req: Request, res: Response, next: NextFunction) => {};
 
-
-const readUser = (req: Request, res: Response, next: NextFunction) => {};
-const createWatchlist = (req: Request, res: Response, next: NextFunction) => {};
+// addToWatchlist: This function would add a new item to the user's watchlist.
 const addToWatchlist = (req: Request, res: Response, next: NextFunction) => {};
+
+// removeFromWatchlist: This function would remove an item from the user's watchlist.
 const removeFromWatchlist = (req: Request, res: Response, next: NextFunction) => {};
+
+// clearWatchlist: This function would remove all items from the user's watchlist.
+const clearWatchlist = (req: Request, res: Response, next: NextFunction) => {};
+
+// updateWatchlist: This function would allow the user to update the details of an item in their watchlist, such as changing its name or description.
+const updateWatchlist = (req: Request, res: Response, next: NextFunction) => {};
+
+// getWatchlistItem: This function would retrieve a specific item from the user's watchlist based on its ID.
+const getWatchlistItem = (req: Request, res: Response, next: NextFunction) => {};
+
+// getWatchlistCount: This function would return the total number of items in the user's watchlist.
+const getWatchlistCount = (req: Request, res: Response, next: NextFunction) => {};
+
+// These functions would allow you to create a basic Watchlist management system in your Node controller. You would also need to implement appropriate database queries or other storage mechanisms to save and retrieve the user's watchlist data
